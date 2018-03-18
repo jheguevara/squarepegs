9aa062e4782604d23bd48e486879d31e72767078
jheguevara <jheguevara@pulpjava.com>
2018-03-17 21:01:56 -0400

    Square with log4j support

---
+++ src/main/java/com/mcnz/Square.java
@@ -0,0 +1,40 @@
+package com.mcnz; 
+import org.apache.log4j.Logger;
+
+/**
+ * The Square class represents 
+ * a four sided 2D shape
+ * @author Cameron McKenzie
+ * @version 1.0
+ * @since 2018-03-01
+ */
+public class Square {
+	
+	static Logger log = Logger.getLogger(Square.class);
+	
+	int height;
+	public Square(){}
+	public Square(int h) {
+		//if (h<0){h=0;}
+		height = h;
+	}
+	public int getHeight(){
+		return height;
+	}
+	public int getWidth() {
+		return height; 
+	}
+	/**
+	Calculates the area of the Square
+	@return a positive int for the area
+	*/
+	public int getArea() {
+		log.info("getting area");
+		return height*height;
+	}
+}
+ 
+
+
+
+
---
+++ src/test/java/com/mcnz/TestSquare.java
@@ -0,0 +1,33 @@
+package com.mcnz; 
+
+import org.junit.Assert;
+import org.junit.Test;
+import com.mcnz.Square; 
+
+public class TestSquare {
+
+	@Test
+	public void testDefaultConstructor() {
+		Square square = new Square();
+		Assert.assertEquals(0, square.getHeight());
+		Assert.assertEquals(0, square.getWidth());
+		//Assert.fail("Forcing a test failure for no reason.");
+
+	}
+	
+	@Test
+	public void testNonDefaultConstructor() {
+		Square square = new Square(10);
+		//Square square = new Square(-10);
+		int height = square.getHeight(); 
+		Assert.assertEquals(square.getWidth(), height);
+		if (height<0){Assert.fail("No negatives.");} 
+	}
+	
+	@Test
+	public void testGetArea() {
+		Square square = new Square(10);
+		int area = square.getArea();
+		Assert.assertEquals(100, area);
+	}
+}
